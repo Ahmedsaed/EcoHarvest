d6af4224668946971ab2c697c326005e3795d420
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-12-07 22:41:00 +0200

    api route to update data

--- API/data.py
+++ API/data.py
@@ -2,6 +2,7 @@ from API import app
 from flask import request, jsonify
 import pandas as pd
 import numpy as np
+from datetime import datetime, timedelta
 
 # Load the CSV file
 data = pd.read_csv('./dummy_data.csv')
@@ -26,3 +27,26 @@ def get_co2_level(n=5):
 @app.route('/api/data/water_ph_value/<int:n>', methods=['GET'])
 def get_water_ph_value(n=5):
     return jsonify(data['water_ph_value'].head(n).to_dict())
+
+@app.route('/api/data/update', methods=['PUT'])
+def update_data():
+    # Assume the JSON format is {'temperature': value, 'light_intensity': value, 'co2_level': value, 'water_ph_value': value}
+    request_data = request.json
+
+
+    # Update the data with the new values
+    data.loc[len(data)] = [
+        datetime(2023, 1, 1),
+        request_data['temperature'],
+        np.random.uniform(100, 1000),
+        np.random.uniform(300, 800),
+        request_data['ph']
+    ]
+
+    return jsonify({"message": "Data updated successfully"}), 200
+
+
+@app.teardown_appcontext
+def save_data(*args, **kwargs):
+    # Save the data to the CSV file
+    data.to_csv('./dummy_data.csv', index=False)
